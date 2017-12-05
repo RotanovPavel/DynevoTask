835622b1805511ad0c5b578706656ddc6573ddfe
ODDpavel <rotanov.pavel@gmail.com>
2017-12-05 18:38:17 +0300

    'Dynevo'

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*
+ * jQuery
+ */
+//=require ../../bower_components/jquery/dist/jquery.js
+//=require ../../bower_components/bootstrap/dist/js/bootstrap.js
+
+
+
+/*
+ * Custom
+ */
+//=include partials/app.js
